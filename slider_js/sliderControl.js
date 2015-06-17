c9e7cf1c0602ef2373082b983ef2d883b896c61d
juventine <andstan@tut.by>
2015-06-17 17:44:36 +0300

    editing code style

--- slider_js/sliderControl.js
+++ slider_js/sliderControl.js
@@ -1,14 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-var options = {
-	visibleImgs : 4, //number of visible images in Viewport
-	isButtonsVisible : true, // Flag: buttons visible or invisible
-	activeImageNumber : 3, // set active number of Image in Viewport
-};
-var elementContainer = document.getElementById('container');
-var elemLeftButton = document.getElementById('leftButton');
-var elemRightButton = document.getElementById('rightButton');
-if (elemLeftButton && elemRightButton) {
-	mySliderPlugin.init( options, elementContainer, elemLeftButton, elemRightButton );
-}
-
+	var options = {
+		visibleImgs : 4, //number of visible images in Viewport
+		isButtonsVisible : true, // Flag: buttons visible or invisible
+		activeImageNumber : 3, // set active number of Image in Viewport
+	};
+	var elementContainer = document.getElementById('container');
+	var elemLeftButton = document.getElementById('leftButton');
+	var elemRightButton = document.getElementById('rightButton');
+		mySliderPlugin.init( options, elementContainer, elemLeftButton, elemRightButton );
 });
